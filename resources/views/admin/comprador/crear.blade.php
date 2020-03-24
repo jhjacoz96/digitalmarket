ff81c0fbdb6fbf669a22e088a260e5a9c71b080b
jhon contreras <jhjacoz96@gmail.com>
2020-03-24 18:19:05 -0430

    2 plantillas listas

---
+++ resources/views/admin/comprador/crear.blade.php
@@ -0,0 +1,25 @@
+@extends('plantilla.admin')
+
+@section('contenido')
+
+<!-- Default box -->
+<div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Title</h3>
+
+    </div>
+    <div class="card-body">
+      Start creating your amazing application!
+    </div>
+    <!-- /.card-body -->
+    <div class="card-footer">
+      Footer
+    </div>
+    <!-- /.card-footer-->
+  </div>
+  <!-- /.card -->
+    
+
+
+@endsection
+

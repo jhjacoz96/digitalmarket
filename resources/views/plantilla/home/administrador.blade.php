eac7fa6901558f9b2a74704f93a1a7ad97656517
Jhon Contreras <jhjacoz96@gmail.com>
2020-06-29 18:35:49 -0430

    refinados

--- resources/views/plantilla/home/administrador.blade.php
+++ resources/views/plantilla/home/administrador.blade.php
@@ -240,7 +240,7 @@
                     <div class="col-md-12">
                       <div class="card card-info">
                         <div class="card-header">
-                          <h3 class="card-title">Medios de pagos mas usados por mes</h3>
+                          <h3 class="card-title">Medios de pagos mas usados</h3>
                           <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                             </button>
